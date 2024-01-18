881e3ebf1c9f1427435372433e2941ca455daa90
srinath <srinath.dash05@gmail.com>
2024-01-18 23:07:46 +0530

    new changes 18 jan 2024

---
+++ src/main/java/CGI/PallindromeNumber.java
@@ -0,0 +1,33 @@
+package CGI;
+
+import java.util.Scanner;
+
+public class PallindromeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int reverse =0 , num , rem;
+		
+		Scanner sc = new Scanner(System.in);
+		num = sc.nextInt();
+		
+		int temp;
+		temp=num;
+		
+		while(num!=0) {
+			rem = num%10;
+			reverse = reverse*10+rem;
+			num=num/10;
+		}
+		System.out.println(reverse);
+		
+		if(reverse==temp) {
+			System.out.println("palindrome");
+		}
+		else
+			System.out.println("no");
+
+	}
+
+}
